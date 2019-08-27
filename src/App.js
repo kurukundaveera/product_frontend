e82c06e77796baf95d199310ee8aad848ede861a
squard4ing <54015743+squard4ing@users.noreply.github.com>
2019-08-27 18:31:44 +0530

    removed daily monthly

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Header from './components/Header/Header';
 import FileUpload from './components/FileUpload/FileUpload';
 import  BuyProducts from './components/BuyProducts/BuyProducts';
 import ListOfProducts from './components/ListOfProducts/ListOfProducts';
-import DailyAnalysis from './components/Analysis/DailyAnalysis';
-import WeeklyAnalysis from './components/Analysis/WeeklyAnalysis';
-import MonthlyAnalysis from './components/Analysis/MonthlyAnalysis';
 import Analysis from './components/Analysis/Analysis';
 
 function App() {
@@ -18,9 +15,6 @@ function App() {
         <Route path="/fileUpload" component={FileUpload}/>
         <Route path="/listOfProducts" component={ListOfProducts}/>
         <Route path="/buyProducts" component={BuyProducts}/>
-        <Route path="/dailyAnalysis" component={DailyAnalysis}/>
-        <Route path="/weeklyAnalysis" component={WeeklyAnalysis}/>
-        <Route path="/monthlyAnalysis" component={MonthlyAnalysis}/>
         <Route path="/analysis" component={Analysis}/>
        </HashRouter>
    
